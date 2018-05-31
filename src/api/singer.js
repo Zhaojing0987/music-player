d74440418effb8772e853f107081ec3a666609f5
chenbaochao <674181203@qq.com>
2018-05-31 15:43:05 +0800

    Initial commit

---
+++ src/api/singer.js
@@ -0,0 +1,36 @@
+import jsonp from 'common/js/jsonp'
+import {commonParams, options} from './config'
+
+export function getSingerList() {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/v8.fcg'
+  const data = Object.assign({}, commonParams, {
+    channel: 'singer',
+    page: 'list',
+    key: 'all_all_all',
+    pagesize: 100,
+    pagenum: 1,
+    hostUin: 0,
+    notice: 0,
+    platform: 'yqq',
+    needNewCode: 0,
+    format: 'jsonp'
+  })
+
+  return jsonp(url, data, options)
+}
+
+export function getSingerDetail(singerId) {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_track_cp.fcg'
+  const data = Object.assign({}, commonParams, {
+    g_tk: 586819093,
+    hostUni: 0,
+    needNewCode:'yqq',
+    begin:0,
+    num:100,
+    songstatus:1,
+    singermid:singerId,
+    format: 'jsonp'
+  })
+
+  return jsonp(url, data, options)
+}
